86727a4242e2422cf6bd914ef2999e9536ff3c5d
Aarifpremad <aarifpremad@gmail.com>
2025-01-01 10:55:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let app = express();
 app.get("/",(req,res)=>{
     res.send("hello dear")
 })
+app.get("/about",(req,res)=>{
+    res.send("about you self")
+})
 app.listen(4001,()=>{
     console.log("server is started localhost:4001")
 })
